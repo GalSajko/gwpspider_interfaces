07858a0deee911157570b52261ee1138c09e4772
jurck222 <jurck222@gmail.com>
2023-08-10 10:04:30 +0200

    added messages service

--- gwpspider_interfaces/gwp_interfaces_data.py
+++ gwpspider_interfaces/gwp_interfaces_data.py
@@ -36,6 +36,7 @@ TOGGLE_ADDITIONAL_CONTROLLER_MODE_SERVICE = 'toggle_additional_controller_mode_s
 #Server
 SEND_GOAL_SERVICE = 'spider_goal_service'
 SET_WATERING_SUCCESS_SERVICE = 'set_watering_success_service'
+MESSAGE_SERVICE = 'message_service'
 
 # Dynamixel
 TOGGLE_MOTORS_TORQUE_SERVICE = 'toggle_motors_torque_service'
